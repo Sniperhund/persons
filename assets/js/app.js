e54dfb88a5cbc2d1f1d13aae132b4170c9a77bde
Lucas Täkker <lucastakker@gmail.com>
2024-08-19 12:56:35 +0200

    chore: Refactor app.js to use async/await for fetching persons data

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,91 +11,61 @@
 /**
  * @type {Person[]}
  */
-const persons = [
-    {
-        image: "https://picsum.photos/200",
-        name: "Lucas Täkker",
-        username: "lucasskt",
-        email: "not@sharing.lol",
-    },
-    {
-        image: "https://picsum.photos/200",
-        name: "Lucas Täkker",
-        username: "lucasskt",
-        email: "not@sharing.lol",
-        useMaps: true,
-        mapCoords: [56.44917, 9.4303],
-    },
-    {
-        image: "https://picsum.photos/200",
-        name: "Lucas Täkker",
-        username: "lucasskt",
-        email: "not@sharing.lol",
-    },
-    {
-        image: "https://picsum.photos/200",
-        name: "Lucas Täkker",
-        username: "lucasskt",
-        email: "not@sharing.lol",
-        useMaps: true,
-        mapCoords: [60.44917, 11.4303],
-    },
-    {
-        image: "https://picsum.photos/200",
-        name: "Lucas Täkker",
-        username: "lucasskt",
-        email: "not@sharing.lol",
-    },
-    {
-        image: "https://picsum.photos/200",
-        name: "Lucas Täkker",
-        username: "lucasskt",
-        email: "not@sharing.lol",
-        useMaps: true,
-        mapCoords: [56.46407, 9.44836],
-    },
-]
+let app = {}
 
-const outputContainer = document.querySelector("#grid")
+app.init = async () => {
+    const persons = await fetch("/assets/data/persons.json").then((response) =>
+        response.json()
+    )
 
-persons.forEach((person, i) => {
-    const personArticle = document.createElement("article")
+    const outputContainer = document.querySelector("#grid")
 
-    personArticle.innerHTML = `
-        <div class="background">
-            ${
-                person.useMaps
-                    ? `<div id="map-${i}" style="width: 100%; height: 100%; border-radius: 30px 30px 0 0;"></div>`
-                    : ``
+    const personTemplate = (person, i) =>
+        `<article>
+            <div class="background">
+                ${
+                    person.useMaps
+                        ? `<div id="map-${i}" style="width: 100%; height: 100%; border-radius: 30px 30px 0 0;"></div>`
+                        : ``
+                }
+            </div>
+            <img class="photo" src="${person.image}" alt="Image" />
+            <div class="text">
+                <h2>${person.name}</h2>
+                <p>${person.username}</p>
+                <p class="email">${person.email}</p>
+                <a href="#">se website</a>
+            </div>
+        </article>`
+
+    persons.forEach((person, i) => {
+        console.log(personTemplate(person, i))
+
+        outputContainer.insertAdjacentHTML(
+            "beforeend",
+            personTemplate(person, i)
+        )
+
+        if (person.useMaps) {
+            if (!person.mapCoords) {
+                console.error(`Map coords is undefined for: ${person.name}`)
+                return
             }
-        </div>
-        <img class="photo" src="${person.image}" alt="Image" />
-        <div class="text">
-            <h2>${person.name}</h2>
-            <p>${person.username}</p>
-            <p class="email">${person.email}</p>
-            <a href="#">se website</a>
-        </div>`
 
-    outputContainer.appendChild(personArticle)
+            var map = L.map(`map-${i}`, {
+                center: person.mapCoords,
+                zoom: 14,
+                zoomControl: false,
+                doubleClickZoom: false,
+                dragging: false,
+            })
 
-    if (person.useMaps) {
-        if (!person.mapCoords) {
-            console.error(`Map coords is undefined for: ${person.name}`)
-            return
+            L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+                maxZoom: 14,
+                detectRetina: true,
+            }).addTo(map)
         }
+    })
+}
 
-        var map = L.map(`map-${i}`, {
-            center: person.mapCoords,
-            zoom: 14,
-            zoomControl: false,
-            doubleClickZoom: false,
-            dragging: false,
-        })
-
-        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            maxZoom: 14,
-            detectRetina: true,
-        }).addTo(map)
-    }
-})
+app.init()
